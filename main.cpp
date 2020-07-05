366aa37e72cf977d9cc375ec5cbb865b81c35ef4
Yusuke Arai <yusuke.arailly@gmail.com>
2020-07-05 22:57:49 +0900

    :+1: complete: main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
 #include <arailib.hpp>
 #include <aknng.hpp>
 
+using namespace std;
+using namespace arailib;
+using namespace aknng;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    string base_dir = "/home/arai/workspace/";
+    string data_path = base_dir + "dataset/sift/data1m/";
+
+    const auto start = get_now();
+
+    int degree = 25, n = 1000;
+    AKNNG aknng(degree);
+    aknng.build(data_path, n);
+
+    string save_path = base_dir + "index/aknng/sift/data1000k-d25/";
+    aknng.save(save_path);
+
+    const auto end = get_now();
+    cout << get_duration(start, end) / 1000000 / 60 << " [min]" << endl;
 }
