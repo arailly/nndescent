9ac368d4b64ed73fb0de04cae54a0a9dac539bec
Yusuke Arai <yusuke.arailly@gmail.com>
2020-07-05 09:58:40 +0900

    :+1: implement init neighbors process

--- include/aknng.hpp
+++ include/aknng.hpp
@@ -22,11 +22,19 @@ namespace aknng {
             added[data.id] = true;
         }
 
-//        void add_neighbor(int node_id) {
-//            if (added.find(node_id) != added.end()) return;
-//            added[node_id] = true;
-//            neighbors.push_back(node_id);
-//        }
+        void add_neighbor(const Node& node) {
+            if (added.find(node.id) != added.end()) return;
+            added[node.id] = true;
+            const auto dist = euclidean_distance(data, node.data);
+            neighbors.emplace(dist, node.id);
+
+            // delete neighbor if over degree
+            if (neighbors.size() > degree) {
+                const auto tail_ptr = --neighbors.cend();
+                neighbors.erase(tail_ptr);
+                added.erase(tail_ptr->second);
+            }
+        }
 
         auto get_n_neighbors() const { return neighbors.size(); }
     };
@@ -34,13 +42,32 @@ namespace aknng {
     struct AKNNG {
         vector<Node> nodes;
         int degree;
+        mt19937 engine;
 
-        AKNNG(int degree) : degree(degree) {}
+        AKNNG(int degree) : degree(degree), engine(42) {}
         auto size() const { return nodes.size(); }
         auto begin() const { return nodes.begin(); }
         auto end() const { return nodes.end(); }
         decltype(auto) operator [] (size_t i) { return nodes[i]; }
         decltype(auto) operator [] (const Node& n) { return nodes[n.id]; }
+
+        void build(string data_path) {
+            const auto dataset = load_data(data_path);
+
+            // init nodes
+            for (auto& data : dataset) {
+                nodes.emplace_back(move(data), degree);
+            }
+
+            // init neighbors
+            uniform_int_distribution<int> dist(0, nodes.size());
+            for (auto& node : nodes) {
+                while (node.neighbors.size() < degree) {
+                    const auto& random_node = nodes[dist(engine)];
+                    node.add_neighbor(random_node);
+                }
+            }
+        }
     };
 }
 
