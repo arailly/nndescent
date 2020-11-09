ea0f7296258749eaa705e8fef3aeea944e56ce0c
arailly <yusuke.arailly@gmail.com>
2020-11-09 13:51:38 +0900

    :+1: any metric

--- main.cpp
+++ main.cpp
@@ -8,14 +8,15 @@ using namespace aknng;
 int main() {
     string base_dir = "/home/arai/workspace/";
     string data_path = base_dir + "dataset/sift/data1m/";
+    string distance = "euclidean";
 
     const auto start = get_now();
 
-    int degree = 25, n = 10;
-    AKNNG aknng(degree);
+    int degree = 50, n = 1000;
+    AKNNG aknng(degree, distance);
     aknng.build(data_path, n);
 
-    string save_path = base_dir + "index/aknng/sift/data10k-d25/";
+    string save_path = base_dir + "index/aknng/sift/data1m-d50/";
     aknng.save(save_path);
 
     const auto end = get_now();
